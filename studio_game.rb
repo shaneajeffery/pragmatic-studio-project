86e42bf319c07fe1d8d47efd9df591d23bbc3fc7
Shane Jeffery <shane.jeffery@mckesson.com>
2021-12-26 10:36:58 -0800

    Finished chapter 7 of ruby -- classes

--- studio_game.rb
+++ studio_game.rb
@@ -1,18 +1,56 @@
 # frozen_string_literal: true
 
-def time
-  current_time = Time.new
-  current_time.strftime('%I:%M:%S')
+class Player
+  def initialize(name, health = 100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health}."
+  end
+
+  # def time
+  #   current_time = Time.new
+  #   current_time.strftime('%I:%M:%S')
+  # end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
 end
 
-def say_hello(player_name, player_health = 100)
-  "I'm #{player_name.capitalize} with a health of #{player_health} as of #{time}."
-end
+player1 = Player.new('moe')
+puts player1
 
-puts say_hello('larry', 60)
-puts say_hello('curly', 125)
-puts say_hello('moe')
-puts say_hello('shemp', 90)
+player2 = Player.new('larry', 60)
+puts player2
+
+player3 = Player.new('curly', 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
+
+
+
+
+#
+# def say_hello(player_name, player_health = 100)
+#   "I'm #{player_name.capitalize} with a health of #{player_health} as of #{time}."
+# end
+#
+# puts say_hello('larry', 60)
+# puts say_hello('curly', 125)
+# puts say_hello('moe')
+# puts say_hello('shemp', 90)
 
 # name1 = 'larry'
 # health1 = 60
