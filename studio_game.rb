34abba9d130b7dcb082f94ab65e7677af439f100
Shane Jeffery <shane.jeffery@mckesson.com>
2021-12-26 10:53:29 -0800

    Finish chapter 8 on class attributes

--- studio_game.rb
+++ studio_game.rb
@@ -6,14 +6,15 @@ class Player
     @health = health
   end
 
-  def to_s
-    "I'm #{@name} with a health of #{@health}."
+  attr_accessor :name, :health
+
+  def score
+    @health + @name.length
   end
 
-  # def time
-  #   current_time = Time.new
-  #   current_time.strftime('%I:%M:%S')
-  # end
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
 
   def blam
     @health -= 10
@@ -26,18 +27,27 @@ class Player
   end
 end
 
-player1 = Player.new('moe')
-puts player1
-
 player2 = Player.new('larry', 60)
+puts player2.name
+player2.name = 'Lawrence'
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
 
-player3 = Player.new('curly', 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
+
+# player1 = Player.new('moe')
+# puts player1
+#
+# player2 = Player.new('larry', 60)
+# puts player2
+#
+# player3 = Player.new('curly', 125)
+# puts player3
+# player3.blam
+# puts player3
+# player3.w00t
+# puts player3
 
 
 
