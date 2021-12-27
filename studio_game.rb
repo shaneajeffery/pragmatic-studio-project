0d732203ff956951863bed25f80d8a38d5bebd6c
Shane Jeffery <shane.jeffery@mckesson.com>
2021-12-26 23:15:06 -0800

    End of chapter 9 object iteracting

--- studio_game.rb
+++ studio_game.rb
@@ -28,17 +28,48 @@ class Player
 end
 
 
+class Game
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+
+    @players.each do |player|
+      3.times { player.w00t }
+      2.times { player.blam }
+      puts player
+    end
+
+  end
+
+end
+
 player1 = Player.new('moe')
 player2 = Player.new('larry', 60)
 player3 = Player.new('curly', 125)
 
-players = [player1, player2, player3]
+knuckleheads = Game.new('Knuckleheads')
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
 
-puts "There are #{players.size} players in the game:"
 
-players.each do |player|
-  puts player
-end
+#
+# players = [player1, player2, player3]
+#
+#
+#
+# players.each do |player|
+#   puts player
+# end
 # player2 = Player.new('larry', 60)
 # puts player2.name
 # player2.name = 'Lawrence'
